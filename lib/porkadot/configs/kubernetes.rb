0ba8a18685f7992619cfd58474487b482850ccad
OTSUKA, Yuanying <yuanying@fraction.jp>
2020-03-20 06:32:11 +0000

    Add recommended labels to kube components

--- lib/porkadot/configs/kubernetes.rb
+++ lib/porkadot/configs/kubernetes.rb
@@ -4,6 +4,9 @@ module Porkadot; module Configs
     include Porkadot::ConfigUtils
     attr_reader :networking
     attr_reader :proxy
+    attr_reader :apiserver
+    attr_reader :controller_manager
+    attr_reader :scheduler
 
     def initialize config
       @config = config
@@ -11,6 +14,9 @@ module Porkadot; module Configs
 
       @networking = Networking.new(config)
       @proxy = Proxy.new(config)
+      @apiserver = Apiserver.new(config)
+      @controller_manager = ControllerManager.new(config)
+      @scheduler = Scheduler.new(config)
     end
 
     def target_path
@@ -21,8 +27,65 @@ module Porkadot; module Configs
       File.join(self.target_path, 'manifests')
     end
 
+    module Component
+      RECOMMENDED_LABEL_PREFIX = 'app.kubernetes.io'
+      def labels
+        {
+          "#{RECOMMENDED_LABEL_PREFIX}/name": self.component_name,
+          "#{RECOMMENDED_LABEL_PREFIX}/component": self.component_name,
+          "#{RECOMMENDED_LABEL_PREFIX}/instance": "#{self.component_name}-porkadot",
+          "#{RECOMMENDED_LABEL_PREFIX}/version": self.config.k8s.kubernetes_version,
+          "#{RECOMMENDED_LABEL_PREFIX}/part-of": 'kubernetes',
+          "#{RECOMMENDED_LABEL_PREFIX}/managed-by": 'porkadot',
+        }
+      end
+    end
+
+    class Apiserver
+      include Porkadot::ConfigUtils
+      include Component
+
+      def initialize config
+        @config = config
+        @raw = config.raw.kubernetes.apiserver
+      end
+
+      def component_name
+        'kube-apiserver'
+      end
+    end
+
+    class Scheduler
+      include Porkadot::ConfigUtils
+      include Component
+
+      def initialize config
+        @config = config
+        @raw = config.raw.kubernetes.scheduler
+      end
+
+      def component_name
+        'kube-scheduler'
+      end
+    end
+
+    class ControllerManager
+      include Porkadot::ConfigUtils
+      include Component
+
+      def initialize config
+        @config = config
+        @raw = config.raw.kubernetes.controller_manager
+      end
+
+      def component_name
+        'kube-controller-manager'
+      end
+    end
+
     class Proxy
       include Porkadot::ConfigUtils
+      include Component
 
       def initialize config
         @config = config
@@ -35,6 +98,10 @@ module Porkadot; module Configs
           self.raw.config['clientConnection']['kubeconfig'] = kubeconfig
         end
         self.raw.config.to_hash.to_yaml
+      end
+
+      def component_name
+        'kube-proxy'
       end
     end
 
